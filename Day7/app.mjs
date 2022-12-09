a2ebabc548fd41268728faf979a45787af454107
SimonHEIG <simon.meia@heig-vd.ch>
2022-12-09 17:24:57 +0100

    day 7

--- Day7/app.mjs
+++ Day7/app.mjs
@@ -7,8 +7,7 @@ readFile(filename, 'utf-8', function (err, data) {
         return console.warn(`Could not read file because: ${err.message}`);
     }
 
-    const logs = data.split("\r\n")
-
+    const logs = data.split("\n")
     // console.log(logs);
 
     let fileSystem = {
@@ -19,9 +18,11 @@ readFile(filename, 'utf-8', function (err, data) {
 
     let currentPath = []
     let listing = false
-    let sum = 0
+    let part1 = 0
+    // console.log(logs);
 
     for (const log of logs) {
+        // console.log(log.slice(0, 4));
         if (log.slice(0, 4) == '$ cd') {
             listing = false
             const direction = log.split(' ')[2]
@@ -29,24 +30,24 @@ readFile(filename, 'utf-8', function (err, data) {
             if (direction == '..') {
                 const content = log.split(' ')
 
+                // Ajout du total du dossier à la somme
                 let cursor = currentPath.toString()
                 let currentDirectory = cursor.split(",").reduce(function (obj, prop) {
                     return obj && obj[prop];
                 }, fileSystem);
-
-
                 const total = currentDirectory.total
                 if(total <= 100000){
-                    sum += total
+                    part1 += total
                 }
 
+                // Remonter d'un niveau
                 currentPath.pop()
 
+                // Ajout de la total du dossier enfant au total du dossier parrent
                 cursor = currentPath.toString()
                 currentDirectory = cursor.split(",").reduce(function (obj, prop) {
                     return obj && obj[prop];
                 }, fileSystem);
-
                 currentDirectory.total += total
 
             } else {
@@ -69,7 +70,7 @@ readFile(filename, 'utf-8', function (err, data) {
             if (content[0] == "dir") {
                 currentDirectory[content[1]] = {}
                 currentDirectory[content[1]]['total'] = 0
-            }else if(content[0] != "$"){
+            } else if (content[0] != "$") {
                 currentDirectory[content[1]] = parseInt(content[0])
                 currentDirectory.total += parseInt(content[0])
             }
@@ -77,7 +78,18 @@ readFile(filename, 'utf-8', function (err, data) {
     }
 
     // console.log(fileSystem);
-    console.log(JSON.stringify(fileSystem, null, 2));
-    console.log(sum);
+    // console.log(JSON.stringify(fileSystem, null, 4));
+    // console.log(part1);
+    console.log(fileSystem['/'].total);
+
+    let totalDiskSpace = 70000000               // 70 000 000
+    let diskSpaceUsed = fileSystem['/'].total   // 43 598 596
+    let diskSpaceNeeded = 30000000              // 30 000 000
+
+    let spaceToClear = diskSpaceUsed + diskSpaceNeeded - totalDiskSpace // 3 598 596
+
+    console.log(spaceToClear);
+
+
 
 });
